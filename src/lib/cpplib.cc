6db768d3284ae14b4fa8e376e202457542317eec
tieming <tiemings@usc.edu>
2020-11-17 15:30:50 -0800

    modify q2

--- src/lib/cpplib.cc
+++ src/lib/cpplib.cc
@@ -7,19 +7,7 @@ bool CPPLib::canFinish(int numCourses, std::vector<std::pair<int, int>> &prerequ
 }
 // Question 2
 std::vector<std::vector<std::string>> CPPLib::NQueens (int n){
-    
+
 }
 // Question 3
 
-// Question 4
-void SinglyLinkedList::sortList() {
-    
-}
-
-// Question 5
-std::vector<char> CPPLib::ShortestPath(std::vector<char> &vertices,
-                               std::vector<std::pair<char, char>> &edges,
-                               char node_1, char node_2) {
-                        
-
-}
